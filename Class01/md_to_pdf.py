c347514f731354c2a9c998d9043e5e52da6b98dc
Ashraf Fatema <ashraf@Ashrafs-Mac-mini.local>
2026-01-26 20:19:41 +0530

    Class Notes

--- Class01/md_to_pdf.py
+++
@@ -1,9 +0,0 @@
-from markdown_pdf import MarkdownPdf, Section
-
-pdf = MarkdownPdf()
-# Replace 'your_content_here' with the content of your .md file or use the add_section method
-with open("/notes.md", "r", encoding="utf-8") as f:
-    markdown_content = f.read()
-
-pdf.add_section(Section(markdown_content))
-pdf.save("/Users/ashraf/Math Code Academy/Eshan/notes.pdf")
--- Class01/my_math.py
+++
@@ -1,9 +0,0 @@
-
-
-def add (a,b) :
-
-    return a + b + 20
-
-def sub(a,b) :
-
-    return a - b + 20
